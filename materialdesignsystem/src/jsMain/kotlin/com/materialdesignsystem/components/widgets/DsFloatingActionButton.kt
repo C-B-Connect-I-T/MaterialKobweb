b9700e36bfe4948988a146a6722d45aa22334508
ShaHar91 <bollachristiano@gmail.com>
2025-09-07 10:45:23 +0200

    Rework the Floating action button a bit

--- materialdesignsystem/src/jsMain/kotlin/com/materialdesignsystem/components/widgets/DsFloatingActionButton.kt
+++ materialdesignsystem/src/jsMain/kotlin/com/materialdesignsystem/components/widgets/DsFloatingActionButton.kt
@@ -1,36 +1,43 @@
 package com.materialdesignsystem.components.widgets
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.modifiers.bottom
-import com.varabyte.kobweb.compose.ui.modifiers.fontSize
-import com.varabyte.kobweb.compose.ui.modifiers.position
-import com.varabyte.kobweb.compose.ui.modifiers.right
-import com.varabyte.kobweb.silk.components.forms.ButtonVars
-import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
-import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import com.materialdesignsystem.extensions.ButtonSizeXL
-import org.jetbrains.compose.web.css.Position
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.silk.components.forms.ButtonSize
+import com.varabyte.kobweb.silk.components.forms.ButtonVars
 import org.jetbrains.compose.web.css.minus
-import org.jetbrains.compose.web.css.px
 
 @Composable
-fun FGFloatingActionButton(
+fun DsFloatingActionButton(
+    modifier: Modifier,
+    size: ButtonSize = ButtonSizeXL,
+    onClick: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    FilledIconButton(
+        modifier = modifier,
+        size = size,
+        onClick = onClick,
+        content = content
+    )
+}
+
+@Composable
+fun DsAddFloatingActionButton(
+    modifier: Modifier,
+    size: ButtonSize = ButtonSizeXL,
     onClick: () -> Unit
 ) {
-    val breakpoint = rememberBreakpoint()
-
-    FilledIconButton(
-        modifier = Modifier
-            .position(Position.Fixed)
-            .right(if (breakpoint >= Breakpoint.MD) 100.px else 36.px)
-            .bottom(150.px),
-        size = ButtonSizeXL,
-        onClick = onClick
-    ) {
-        DsMaterialSymbols(
-            modifier = Modifier.fontSize(ButtonVars.Height.value() - ButtonVars.PaddingHorizontal.value()),
-            icon = "add"
-        )
-    }
+    DsFloatingActionButton(
+        modifier,
+        size,
+        onClick = onClick,
+        content = {
+            DsMaterialSymbols(
+                modifier = Modifier.fontSize(ButtonVars.Height.value() - ButtonVars.PaddingHorizontal.value()),
+                icon = "add"
+            )
+        }
+    )
 }
